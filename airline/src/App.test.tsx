c14e436076ab0cd10b7b0a9b7e2f82bae4d0ef31
AnuchitO <3372362+AnuchitO@users.noreply.github.com>
2026-03-12 15:00:11 +0700

    remove demo ariline not use

--- airline/src/App.test.tsx
+++
@@ -1,7 +0,0 @@
-import { render, screen } from '@testing-library/react'
-
-test('example vitest', () => {
-  render(<div><button>Click me</button></div>)
-
-  expect(screen.getByText('Click me')).toBeInTheDocument()
-})
--- airline/src/App.tsx
+++
@@ -1,9 +0,0 @@
-// import CheckInPage from './components/CheckInPage'
-
-import CheckInPage from "./components/CheckInPage"
-
-function App() {
-  return <CheckInPage />
-}
-
-export default App
